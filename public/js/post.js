31ed20aa2da5d4839a923fcffbcec8ca7d8df507
JeremyF <jfulbright@gmail.com>
2022-12-23 09:07:01 -0700

    Finished CRUD operations for comments

--- public/js/post.js
+++ public/js/post.js
@@ -1,6 +1,11 @@
 let deletePost;
+let deleteComment;
+let postComment;
 
 deletePost = document.querySelectorAll('.post-list');
+deleteComment = document.querySelectorAll('.comment-list');
+postComment = document.querySelectorAll('.comment-form');
+document.querySelector('.post-form').addEventListener('submit', newFormHandler); // don't call if not logged in
 
 const newFormHandler = async (event) => {
   event.preventDefault();
@@ -25,13 +30,37 @@ const newFormHandler = async (event) => {
     if (response.ok) {
       document.location.replace('/');
     } else {
+      alert('Failed to create a Post');
+    }
+  }
+};
+
+const newCommentHandler = async (event) => {
+  event.preventDefault();
+  const body = event.target.querySelector('#comment_body').value.trim();
+  const comment_post_id = event.target.getAttribute('data-id');
+
+  if (body) {
+    const response = await fetch(`/api/comments`, {
+      method: 'POST',
+      body: JSON.stringify({
+        comment_body: body,
+        post_id: comment_post_id,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
       alert('Failed to create an Item');
     }
   }
 };
 
 const delButtonHandler = async (event) => {
-  console.log('function called');
   if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
 
@@ -47,10 +76,32 @@ const delButtonHandler = async (event) => {
   }
 };
 
-document.querySelector('.post-form').addEventListener('submit', newFormHandler);
+const delCommentHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+    const response = await fetch(`/api/comments/${id}`, {
+      method: 'DELETE',
+    });
 
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      alert('Failed to delete item');
+    }
+  }
+};
 
 // Iterate through all Post instances and add a listener:
 for (var i = 0; i < deletePost.length; i++) {
   deletePost[i].addEventListener('click', delButtonHandler);
+}
+
+// Iterate through all Comments instances and add a listener:
+for (var i = 0; i < deleteComment.length; i++) {
+  deleteComment[i].addEventListener('click', delCommentHandler);
+}
+
+// Iterate through all New Comment Form instances and add a listener:
+for (var i = 0; i < postComment.length; i++) {
+  postComment[i].addEventListener('submit', newCommentHandler);
 }
